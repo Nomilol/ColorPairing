ada6f9c2b4862bead1e990b196417895c7c5b5f1
Axel <lezinaxel@gmail.com>
2016-10-07 09:46:47 +0200

    Color pairing

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 // good luck !
 var colors = $('.color');
+// Ici on a récupérer les couleurs et elles sont dans un tableau 
 var len = colors.length;
-function addColor(){
+// Ici on donne une variable à la longueur du tableau pour des raisons de performances
 
+function addColor(){
 	
 	for(var i=0;i<len;i++){
 		var bColor = $(colors[i]).data('color');
 		$(colors[i]).css('backgroundColor',bColor);
 		
 	}
-
+// On crée une boucle pour parcourir le tableau
 
 };
 $('.color').click(function(){
 	var bodyColor = $(this).data('color');
 
 	var t = checked();
-	console.log(t);
 	if(t){
 		$('.main').css('color',bodyColor);
 	}else{
 		$('body').css('backgroundColor',bodyColor);
 		
 	};
-
+// On crée notre condition qui modifie ou la couleur du body ou la couleur du texte selon si notre fonction est vérifiée
 
 });
 
@@ -32,7 +33,7 @@ function checked(){
 	return $('#modify-texte').is(':checked');
 
 }
-
+// On crée une fonction qui return true ou false selon si la case est cochée
 
 
 
