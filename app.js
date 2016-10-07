625b1488401c8651b06de4a9781b3af902183e86
Axel <lezinaxel@gmail.com>
2016-10-07 12:05:21 +0200

    Remaniement du code

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-// good luck !
-var colors = $('.color');
-// Ici on a récupérer les couleurs et elles sont dans un tableau 
-var len = colors.length;
-// Ici on donne une variable à la longueur du tableau pour des raisons de performances
 
 function addColor(){
+	var colors = $('.color');
+	var len = colors.length;
 	
 	for(var i=0;i<len;i++){
-		var bColor = $(colors[i]).data('color');
-		$(colors[i]).css('backgroundColor',bColor);
-		
+		var backgroundColor = $(colors[i]).data('color');
+		$(colors[i]).css('backgroundColor',backgroundColor);
 	}
-// On crée une boucle pour parcourir le tableau
-
 };
+
 $('.color').click(function(){
 	var bodyColor = $(this).data('color');
 
-	var t = checked();
-	if(t){
+	if(checked()){
 		$('.main').css('color',bodyColor);
 	}else{
 		$('body').css('backgroundColor',bodyColor);
-		
 	};
-// On crée notre condition qui modifie ou la couleur du body ou la couleur du texte selon si notre fonction est vérifiée
-
 });
 
 function checked(){
@@ -33,13 +24,15 @@ function checked(){
 	return $('#modify-texte').is(':checked');
 
 }
-// On crée une fonction qui return true ou false selon si la case est cochée
-
-
-
-
 
 addColor();
+
+
+
+
+
+
+
 
 
 
